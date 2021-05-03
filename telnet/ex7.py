f1198a4b360883f778b05409898093ba1b6c8833
SydAsif <sydasif78@gmail.com>
2021-05-03 09:29:33 +0500

    Add ex7,Loop for configuration of multiple devices

---
+++ telnet/ex7.py
@@ -0,0 +1,33 @@
+# Exercise 7: Loop for configuration of multiple devices.
+
+import getpass
+import telnetlib
+
+user = input("Welcome, if authorized \nPlease enter your telnet Username: ")
+password = getpass.getpass()
+
+''' Create a for loop to configure multiple devices,
+also note that we are converting the value on 'IP' 
+to string value using str() command. 
+1st digit is included & last digit will exclude '''
+
+for IP in range (20,23): 
+    HOST = "192.168.100." + str(IP)
+    print ('configuration of 192.168.100.' + str(IP))
+    tn = telnetlib.Telnet(HOST)
+    tn.read_until(b"Username: ")
+    tn.write(user.encode('ascii') + b"\n")
+    if password:
+        tn.read_until(b"Password: ")
+        tn.write(password.encode('ascii') + b"\n")
+    tn.write(b"enable\n") 
+    tn.write(b"cisco\n")  
+    tn.write(b"conf t\n")
+
+    for v in range(2,10):
+        tn.write(b"vlan " + str(v).encode('ascii') + b"\n")
+        tn.write(b"name VLAN_" + str(v).encode('ascii') + b"\n")
+    tn.write(b"end\n")
+    tn.write(b"show vlan br\n\n")
+    tn.write(b"exit\n")
+    print (tn.read_all().decode('ascii'))
