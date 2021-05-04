e5df3430f540153e8e4403d95d4a531377ee3a16
SydAsif <sydasif78@gmail.com>
2021-05-04 10:45:58 +0500

    Add exe4, SSH to multiple devices

---
+++ pexpect/exe4.py
@@ -0,0 +1,23 @@
+# SSH to Devices
+import pexpect
+
+devices = {'CoreSW': {'prompt': 'CoreSW#', 'ip': '192.168.100.20', 'en': 'CoreSW>'}, 
+           'SW1': {'prompt': 'SW1#', 'ip': '192.168.100.21', 'en': 'SW1>'}}
+username = 'admin'
+password = 'cisco'
+
+for device in devices.keys(): 
+    device_prompt = devices[device]['prompt']
+    device_en = devices[device]['en']
+    child = pexpect.spawn('ssh admin@'+ devices[device]['ip'])
+    child.expect('Password: ')
+    child.sendline(password)
+    child.expect(device_en)
+    child.sendline('enable')
+    child.expect('Password: ')
+    child.sendline(password)
+    child.expect(device_prompt)
+    child.sendline('show version | i V')
+    child.expect(device_prompt)
+    print(child.before)
+    child.sendline('exit')
