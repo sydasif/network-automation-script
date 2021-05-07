b42ed7af172193ccb798a7fd6404bc8b328f606d
SydAsif <sydasif78@gmail.com>
2021-05-07 11:17:36 +0500

    indent error

--- netmiko/ex2.py
+++ netmiko/ex2.py
@@ -23,10 +23,10 @@ SW2 = {
 }
 switches = [CoreSW, SW1, SW2]
 for devices in switches:
- net_connect = ConnectHandler(**devices)
- net_connect.enable()
- for n in range (10, 15):
-    print("Creating VLAN " + str(n))
-    config_commands = ['vlan ' + str(n), 'name DevOps_VLAN ' + str(n)]
-    output = net_connect.send_config_set(config_commands)
-    print(output)
+   net_connect = ConnectHandler(**devices)
+   net_connect.enable()
+   for n in range (10, 15):
+      print("Creating VLAN " + str(n))
+      config_commands = ['vlan ' + str(n), 'name DevOps_VLAN ' + str(n)]
+      output = net_connect.send_config_set(config_commands)
+      print(output)
