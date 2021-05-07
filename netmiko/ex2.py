77a01457a89e7c9e4fdd17a6eb1ddc6c26e62661
SydAsif <sydasif78@gmail.com>
2021-05-07 11:09:34 +0500

    remove last comma in dict

--- netmiko/ex2.py
+++ netmiko/ex2.py
@@ -5,21 +5,21 @@ CoreSW = {
  'ip': '192.168.100.20',
  'username': 'admin',
  'password': 'cisco',
- 'secret' : 'cisco',
+ 'secret' : 'cisco'
 }
 SW1 = {
  'device_type': 'cisco_ios',
  'ip': '192.168.100.21',
  'username': 'admin',
  'password': 'cisco',
- 'secret' : 'cisco',
+ 'secret' : 'cisco'
 }
 SW2 = {
  'device_type': 'cisco_ios',
  'ip': '192.168.100.22',
  'username': 'admin',
  'password': 'cisco',
- 'secret' : 'cisco',
+ 'secret' : 'cisco'
 }
 switches = [CoreSW, SW1, SW2]
 for devices in switches:
