64e8e6eab93d3bde1dc339d74010e8350046de28
Syed Asif <sydasif78@gmail.com>
2021-06-23 11:27:53 +0500

    A simple example illustrating typical use

---
+++ telnet/telnet_ex1.py
@@ -0,0 +1,22 @@
+# Telnet Example
+
+import getpass
+import telnetlib
+
+HOST = "192.168.100.20"
+user = input("Enter your Username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"term length 0\n")
+tn.write(b"sh ip int bri\n")
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
