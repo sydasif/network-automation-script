d8a4d7c6c15f9a9e25c43b6b99f3d517b19ca980
Syed Asif <sydasif78@gmail.com>
2021-06-23 16:09:04 +0500

    rename all file in paramiko folder

--- paramiko/ex3_vlan_loop.py
+++ paramiko/ex3_vlan_loop.py
@@ -10,16 +10,7 @@ ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh_client.connect(hostname=ip_address,username=username,password=password)
 
 print ("Successful connection", ip_address)
-
 remote_connection = ssh_client.invoke_shell()
-
-remote_connection.send("configure terminal\n")
-remote_connection.send("int loop 0\n")
-remote_connection.send("ip address 1.1.1.1 255.255.255.255\n")
-remote_connection.send("int loop 1\n")
-remote_connection.send("ip address 2.2.2.2 255.255.255.255\n")
-remote_connection.send("router ospf 1\n")
-remote_connection.send("network 0.0.0.0 255.255.255.255 area 0\n")
 
 for n in range (2,21):
     print ("Creating VLAN " + str(n))
