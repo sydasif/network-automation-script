d1cf0cdb505d47fb02415b9d80125a9032ef34ca
Syed Asif <sydasif78@gmail.com>
2021-06-23 14:28:55 +0500

    update

---
+++ paramiko/paramiko_device_loop.py
@@ -0,0 +1,33 @@
+import paramiko
+import time
+
+username = 'admin'
+password = 'cisco'
+
+for sw in range(20,23):
+    ip = "192.168.100." + str(sw)
+    print ('\n Connecting to the device ------> ' + ip)
+    
+    SESSION = paramiko.SSHClient()
+    SESSION.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    SESSION.connect(ip,port=22,
+                    username=username,
+                    password=password,
+                    look_for_keys=False,
+                    allow_agent=False)
+    DEVICE_ACCESS = SESSION.invoke_shell()
+    
+    DEVICE_ACCESS.send(b'config t\n')
+    
+    for N in range (2,5):
+        DEVICE_ACCESS.send('int lo ' +str(N) + '\n')
+        DEVICE_ACCESS.send('ip address 1.1.1.' +str(N) + ' 255.255.255.255\n')  
+
+    time.sleep(5)
+    DEVICE_ACCESS.send(b'do term length 0\n')
+    DEVICE_ACCESS.send(b'do show ip int brief\n')
+    time.sleep(3)
+    output = DEVICE_ACCESS.recv(65000)
+    print (output.decode('ascii'))
+
+    SESSION.close()
