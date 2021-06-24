0bf86641f9145ad1496d9c2137ec5c938d495663
sydasif <sydasif78@gmail.com>
2021-06-24 18:12:49 +0500

    add ex12 for timestamp

---
+++ paramiko/ex12.py
@@ -0,0 +1,32 @@
+import paramiko
+import datetime, time
+
+username = 'admin'
+password = 'cisco'
+
+tnow = datetime.datetime.now().replace(microsecond=0)
+TFORMAT = '{:%d-%m-%Y_%H:%M:%S}'.format(tnow)
+
+list = open ('device')
+for sw in list:
+    sw = sw.strip()
+    print ('\nConnecting to the device ------> ' + sw)
+    
+    SESSION = paramiko.SSHClient()
+    SESSION.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    SESSION.connect(sw,port=22,
+                    username=username,
+                    password=password,
+                    look_for_keys=False,
+                    allow_agent=False)
+    DEVICE_ACCESS = SESSION.invoke_shell()
+    DEVICE_ACCESS.send('copy running-config tftp://192.168.100.100' + '\n\n')
+    DEVICE_ACCESS.send(sw + '_{}'.format(TFORMAT) + '\n')
+    time.sleep(5)
+    output = DEVICE_ACCESS.recv(65000)
+    time.sleep(3)
+    print ()
+    print ('Device backup is complete =======>> ' + sw + '\n')
+    print ('#' * 50)
+    print ()
+    SESSION.close()
