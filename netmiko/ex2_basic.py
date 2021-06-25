9b96bf954d886c6b9587139d3f9db6f890a8e90e
sydasif <sydasif78@gmail.com>
2021-06-25 10:11:14 +0500

    add basic netmiko config example

---
+++ netmiko/ex2_basic.py
@@ -0,0 +1,25 @@
+# Basic config_commands sample
+from netmiko import ConnectHandler
+from getpass import getpass
+
+password = getpass()
+
+CoreSW = {
+    'ip':   '192.168.100.20',
+    'username': 'admin',
+    'password': 'cisco',
+    'device_type': 'cisco_ios',
+}
+
+net_connect = ConnectHandler(**CoreSW)
+
+#Execute configuration change commands (will automatically enter into config mode)
+config_commands = [ 'int lo0',
+                    'ip add 1.1.1.1 255.255.255.255',
+                    'no shut' ]
+output = net_connect.send_config_set(config_commands)
+print(output)
+
+#Execute show commands
+output = net_connect.send_command('show ip int brief')
+print(output)
