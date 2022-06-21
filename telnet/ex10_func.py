e3eea709e68a682282ba93a283e1f285a77dad71
Syed Asif <76906817+sydasif@users.noreply.github.com>
2022-06-21 21:33:08 +0500

    Update ex10_func.py

--- telnet/ex10_func.py
+++ telnet/ex10_func.py
@@ -1,12 +1,24 @@
-# Python telnet 
+# Python telnetlib  
 import getpass
 import telnetlib
 
-commands = ["show ip int bri", "exit"]
+# my function to add commands in one list
+def send_cmd(cmds):
+    for cmd in cmds:
+        tn.write(cmd.encode('ascii') + b"\n")
+    output = tn.read_all().decode('ascii')
+    print(output)
+    
+my_cmds = ["terminal length 0",
+        "sh version",
+        "sh ip int bri",
+        "exit"]
 
-IP = '192.168.138.9'
+IP = '192.168.10.10'
+
 user = input("Enter your username :")
 password = getpass.getpass()
+
 tn = telnetlib.Telnet(IP)
 tn.read_until(b"Username: ")
 tn.write(user.encode("ascii") + b"\n")
@@ -14,11 +26,4 @@ if password:
     tn.read_until(b"Password: ")
     tn.write(password.encode("ascii") + b"\n")
 
-def send_cmd(commands):
-    for command in commands:
-        tn.write(command.encode('ascii') + b"\n")
-    output = tn.read_all().decode('ascii')
-    print(output)
-
-
-send_cmd(commands)
+send_cmd(my_cmds)
