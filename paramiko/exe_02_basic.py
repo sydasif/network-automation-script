2f93403b8572f5d12aee7ca0000b94f5584f33cf
sydasif <sydasif78@gmail.com>
2022-06-22 11:37:12 +0500

    remove recv(5000)

--- paramiko/exe_02_basic.py
+++ paramiko/exe_02_basic.py
@@ -7,7 +7,6 @@ connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 # The next few lines invoke a new interactive shell from the connection
 connection.connect('192.168.100.20', username='admin1', password='cisco1', look_for_keys=False, allow_agent=False)
 new_connection = connection.invoke_shell()
-new_connection.recv(5000)
 
 # Send command to the remote device
 new_connection.send("sh ip int bri\n")
