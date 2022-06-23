596a6fa267e674a73830b6c4cbe53633b08a8ab8
sydasif <sydasif78@gmail.com>
2022-06-23 11:57:59 +0500

    add utf-8 to convert bytes into string

--- pexpect/exe1.py
+++ pexpect/exe1.py
@@ -4,25 +4,32 @@ returned string from the remote device.
 2. The sendline() method indicates which words should be sent to the
 remote device as the command.
 """
+""" 
+1. The expect() method in Pexpect process is an indicator for the 
+returned string from the remote device.
+2. The sendline() method indicates which words should be sent to the
+remote device as the command.
+"""
 import pexpect
 
-child = pexpect.spawn('telnet 192.168.100.20')
+child = pexpect.spawn('telnet 192.168.10.10')
 child.expect('Username: ')  # child.expect('[Uu]sername: ')
 child.sendline('admin')
 child.expect('Password: ')  # child.expect(['Password', 'password'])
 child.sendline('cisco')
-child.expect('CoreSW>')
+child.expect('S1>')
 child.sendline('enable')
 child.expect('Password: ')
 child.sendline('cisco')
-child.expect('CoreSW#')
+child.expect('S1#')
 child.sendline('show version | i V')
-child.expect('CoreSW#')
-# print out the sendline('show version | i V') before expect('CoreSW#') line
-print(child.before)
-# print out the expect('CoreSW#') line which is 'CoreSW#' 
-print(child.after) 
+child.expect('S1#')
+# convert it to a string
+show_output = child.before.decode('utf-8')
+# print out the command 
+print(show_output) 
 child.sendline('exit')
+
 
 # Read the Pexpect docs at 
 # https://pexpect.readthedocs.io/en/stable/index.html
