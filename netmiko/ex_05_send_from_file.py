3b8c32f73b4d139501b14961012cccfa5e93caeb
sydasif <sydasif78@gmail.com>
2022-06-25 12:31:27 +0500

    update ex_05 and add config_file.cfg

--- netmiko/ex_05_send_from_file.py
+++ netmiko/ex_05_send_from_file.py
@@ -3,15 +3,15 @@
 from netmiko import ConnectHandler
 from getpass import getpass
 
-device1 = {
+S1 = {
     "device_type": "cisco_ios",
-    "host": "192.168.100.20",
+    "host": "192.168.10.10",
     "username": "admin",
     "password": getpass(),
 }
 
-cfg_file = "config1.txt"
-with ConnectHandler(**device1) as net_connect:
+cfg_file = "config_file.cfg"
+with ConnectHandler(**S1) as net_connect:
     output = net_connect.send_config_from_file(cfg_file)
     output += net_connect.save_config()
 
