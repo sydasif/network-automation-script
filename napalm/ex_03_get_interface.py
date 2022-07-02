526f031bd2b965cc875953eafba9d665da07bd1b
“sydasif” <sydasif78@gmail.com>
2022-07-02 18:31:16 +0500

    add exe 03 get_interface

---
+++ napalm/ex_03_get_interface.py
@@ -0,0 +1,12 @@
+import json
+from napalm import get_network_driver
+
+driver = get_network_driver('ios')
+device = driver('192.168.10.10', 'admin', 'cisco')
+device.open()
+
+output = device.get_interfaces()
+print(json.dumps(output, indent=4))
+
+device.close()
+print('Done.....')
