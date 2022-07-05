2a35b31cc71972542f54d792c063ce67fbf935f4
“sydasif” <sydasif78@gmail.com>
2022-07-05 09:00:23 +0500

    to connect multiple device napalm script

---
+++ napalm/ex_05_multi_dev_list.py
@@ -0,0 +1,33 @@
+import napalm
+import json
+
+def main():
+
+    driver_ios = napalm.get_network_driver("ios")
+    device_list = [["192.168.10.10","ios", "switch"],["192.168.10.11", "ios", "router"]]
+ 
+    network_devices = []
+    for device in device_list:
+        network_devices.append(
+                driver_ios(
+                hostname = device[0],
+                username = "admin",
+                password = "cisco"
+                )
+                 )
+
+    for device in network_devices:
+        print("#"*40)
+        print("Connecting to {} ...".format(device.hostname))
+        device.open()
+ 
+        print("Getting device facts")
+        device_facts = device.get_facts()
+ 
+        device.close()
+        print("#"*40)
+        print(json.dumps(device_facts, indent=4))
+
+if __name__ == '__main__':
+    main()
+
