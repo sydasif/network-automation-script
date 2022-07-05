0d328f9858683b402ac98eaa44b9b5d193bd2e68
“sydasif” <sydasif78@gmail.com>
2022-07-05 08:53:05 +0500

    add bgp napalm script

---
+++ napalm/ex_05_bgp_ne.py
@@ -0,0 +1,12 @@
+import json
+from napalm import get_network_driver
+
+driver = get_network_driver('ios')
+device = driver('192.168.122.72', 'david', 'cisco')
+device.open()
+
+output = device.get_bgp_neighbors()
+print (json.dumps(output, indent=4))
+
+device.close()
+
