81eb2705bca8a000cd9b64f75cf3effb93b1511c
“sydasif” <sydasif78@gmail.com>
2022-07-07 12:02:45 +0500

    add script to compare

---
+++ napalm/ex_02_compare_config.py
@@ -0,0 +1,21 @@
+import json
+from napalm import get_network_driver
+driver = get_network_driver('ios')
+iosvl2 = driver('192.168.10.10', 'admin', 'cisco')
+iosvl2.open()
+
+print ('loading config from file......192.168.10.10')
+iosvl2.load_merge_candidate(filename='config1.cfg')
+
+diffs = iosvl2.compare_config()
+if len(diffs) > 0:
+    print(diffs)
+    iosvl2.commit_config()
+else:
+    print('No changes required.')
+    iosvl2.discard_config()
+
+iosvl2.close()
+
+
+
