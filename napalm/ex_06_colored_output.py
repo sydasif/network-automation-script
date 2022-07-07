7157c06e64deb84bf98df7da5b13b076785badd7
“sydasif” <sydasif78@gmail.com>
2022-07-07 08:27:09 +0500

    add colored to script output

---
+++ napalm/ex_06_colored_output.py
@@ -0,0 +1,24 @@
+import json
+from napalm import get_network_driver
+# Add color to the output
+from termcolor import colored
+
+device_ip = ['192.168.10.10', '192.168.10.11']
+
+for ip in device_ip:
+    print()
+    print(colored("Connecting..." + ip, "red"))
+    print()
+    driver = get_network_driver('ios')
+    device = driver(ip, 'admin', 'cisco')
+    device.open()
+
+    output = device.get_facts()
+    config = json.dumps(output, indent=4)
+    print(config)
+
+    device.close()
+    print()
+    print(colored('#'*50, "green"))
+    print()
+
