9e53feea6c2a6b67ea424bee79008f34833a1ffa
“sydasif” <sydasif78@gmail.com>
2022-07-07 12:04:18 +0500

    add script to compare 02 cfg file on multi devices

---
+++ napalm/ex_12_config_multi_devices.py
@@ -0,0 +1,35 @@
+import json
+from napalm import get_network_driver
+
+devicelist = ['192.168.10.10',
+           '192.168.10.11'
+           ]
+
+for ip_address in devicelist:
+    print ("Connecting to " + str(ip_address))
+    driver = get_network_driver('ios')
+    iosv = driver(ip_address, 'admin', 'cisco')
+    iosv.open()
+    iosv.load_merge_candidate(filename='config1.cfg')
+    diffs = iosv.compare_config()
+
+    if len(diffs) > 0:
+        print(diffs)
+        iosv.commit_config()
+    else:
+        print('No ACL changes required.')
+        iosv.discard_config()
+
+    iosv.load_merge_candidate(filename='ospf1.cfg')
+
+    diffs = iosv.compare_config()
+
+    if len(diffs) > 0:
+        print(diffs)
+        iosv.commit_config()
+    else:
+    	print('No OSPF changes required.')
+    	iosv.discard_config()
+
+    iosv.close()
+
