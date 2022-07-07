66dd3b1065c430582375f65eb68dec8a04c2d03f
“sydasif” <sydasif78@gmail.com>
2022-07-07 11:29:01 +0500

    add script for mac and arp table

---
+++ napalm/ex_04_get_arp_a.py
@@ -0,0 +1,13 @@
+import json
+from napalm import get_network_driver
+driver = get_network_driver('ios')
+iosvl2 = driver('192.168.10.10', 'admin', 'cisco')
+iosvl2.open()
+
+ios_output = iosvl2.get_mac_address_table()
+print (json.dumps(ios_output, sort_keys=True, indent=4))
+
+ios_output = iosvl2.get_arp_table()
+print (json.dumps(ios_output, sort_keys=True, indent=4))
+
+iosvl2.close()
