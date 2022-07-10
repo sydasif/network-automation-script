8bf90d578c14e6c449d4a7126ee48ca4e91d83f6
“sydasif” <sydasif78@gmail.com>
2022-07-10 12:10:28 +0500

    add nornir basic script

---
+++ nornir/01_nor_basic.py
@@ -0,0 +1,12 @@
+from nornir import InitNornir
+from nornir_utils.plugins.functions import print_result
+from nornir_napalm.plugins.tasks import napalm_get
+
+nr = InitNornir(
+    config_file="config.yaml", dry_run=True
+)
+
+results = nr.run(
+    task=napalm_get, getters=["facts", "interfaces"]
+)
+print_result(results)
