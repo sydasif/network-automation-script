036565871459d0560cc77e84280f232aa9bf25b9
Syed Asif <sydasif78@gmail.com>
2024-05-25 16:48:39 +0000

    update

--- telnet/ex_01_basic.py
+++ telnet/ex_01_basic.py
@@ -1,9 +1,9 @@
 # This is a basic telnet script
-
 from telnetlib import Telnet
 
-cmd = input('Enter the Cmd: ')  # Enter command
-tn = Telnet('192.168.100.20')  # IP Address of device
+cmd = input('Enter the Command: ')  # Command
+ip = input("Enter IP Address: ")  # IP Address of device
+tn = Telnet(ip)
 
 tn.write(b'admin\n')  
 tn.write(b'cisco\n')
