848e55d0a930998b56326cf9e4ff12832df64bf4
Syed Asif <sydasif78@gmail.com>
2024-05-26 17:05:23 +0000

    update ip

--- paramiko/03_paramiko_vlan_loop.py
+++ paramiko/03_paramiko_vlan_loop.py
@@ -3,7 +3,7 @@ import time
 from getpass import getpass
 
 # Define connection details
-ip_address = "192.168.100.20"
+ip_address = "172.16.10.11"
 username = "admin"
 password = getpass('Enter SSH password: ')
 
@@ -12,7 +12,7 @@ ssh_client = paramiko.SSHClient()
 # Set the policy to automatically add the host key
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 # Establish an SSH connection to the target device
-ssh_client.connect(hostname=ip_address, username=username, password=password)
+ssh_client.connect(hostname=ip_address, username=username, password=password, look_for_keys=False,  allow_agent=False)
 
 print("Successful connection to", ip_address)
 # Invoke an interactive shell session on the remote device
@@ -22,7 +22,7 @@ remote_connection = ssh_client.invoke_shell()
 remote_connection.send("configure terminal\n")
 
 # Loop to create VLANs 2 through 20
-for n in range(2, 21):
+for n in range(2, 10):
     print("Creating VLAN " + str(n))
     remote_connection.send("vlan " + str(n) + "\n")
     remote_connection.send("name Python_VLAN_" + str(n) + "\n")
@@ -32,7 +32,7 @@ for n in range(2, 21):
 remote_connection.send("end\n")
 
 # Wait for the commands to be processed
-time.sleep(3)
+time.sleep(5)
 # Retrieve the output from the buffer
 output = remote_connection.recv(65535)
 # Print the received output after decoding it
