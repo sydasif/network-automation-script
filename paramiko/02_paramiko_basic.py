ca57c368c9c9c2ee93f5a567d8a5858d73c00e8e
Syed Asif <sydasif78@gmail.com>
2024-05-26 16:30:38 +0000

    rename file and add comments

---
+++ paramiko/02_paramiko_basic.py
@@ -0,0 +1,31 @@
+import paramiko
+import time
+
+# Create an instance of the SSHClient class from Paramiko
+connection = paramiko.SSHClient()
+# Set the policy to automatically add the host key
+connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+# Establish an SSH connection to the target device
+connection.connect(
+    '192.168.100.20', 
+    username='admin1', 
+    password='cisco1', 
+    look_for_keys=False, 
+    allow_agent=False
+    )
+
+# Invoke an interactive shell session on the remote device
+new_connection = connection.invoke_shell()
+
+# Send the command to the remote device
+new_connection.send("sh ip int bri\n")
+# Wait for the command to be finished on the remote device before retrieving the output
+time.sleep(3)
+
+# Retrieve the output from the buffer
+output = new_connection.recv(5000)
+# Print the received output after decoding it
+print(output.decode())
+
+# Close the remote connection
+new_connection.close()
